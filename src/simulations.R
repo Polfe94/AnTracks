685a19249f0c5f55a8936a943489f0844b499f6e
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-08-11 15:32:55 +0200

    added a function to perform DTW for several simulations

--- src/simulations.R
+++ src/simulations.R
@@ -50,3 +50,41 @@ align_sequences <- function(df, sim_type = 'gain'){
 	colnames(dt_clean)[colnames(dt_clean) == 'variable'] <- sim_type
 	dt_clean
 }
+
+#' Takes two data tables and performes the DTW function
+#' @param sim a data.table comprising N time series (concatenated with an .id label)
+#' @param ref a data.table comprising M time series (as columns)
+#' @param align if TRUE performs `align_sequences` before running the computations
+#' @param only.average if TRUE only the averaged reference sequence is taken into account
+#' @return a vector of length N x M of DTW distances between the simulated and reference time series
+mass_dtw <- function(sim, ref, align = FALSE, only.average = TRUE){
+	if(align){
+		x <- colnames(sim)
+		if('gain' %in% x){
+			t <- 'gain'
+		} else if(grepl('parameter', x)){
+			t <- 'ps'
+		} else {
+			stop('Could not detect the type of simulation. Try setting align to FALSE.')
+		}
+		sim <- align_sequences(sim, t)
+	}
+	result <- data.frame(.id = seq_len(max(sim[['.id']])))
+	if(only.average){
+		tmp <- lapply(seq_along(result[['.id']]), function(i){
+			k <- dtw::dtw(sim[sim[['.id']] == i, N], ref[['avg']])
+			c(d = k[['distance']], nrm_d = k[["normalizedDistance"]])
+		})
+		result <- setDT(cbind(result, do.call('rbind', tmp)))
+	} else {
+		tmp <- lapply(seq_along(result[['.id']]), function(i){
+			do.call('rbind', lapply(1:ncol(ref), function(j){
+				k <- dtw::dtw(sim[sim[['.id']] == i, N], ref[,..j])
+				c(d = k[['distance']], nrm_d = k[["normalizedDistance"]])
+			}))
+		})
+		result <- setDT(cbind(result, ref = colnames(ref), do.call('rbind', tmp)))
+	}
+
+	result
+}
