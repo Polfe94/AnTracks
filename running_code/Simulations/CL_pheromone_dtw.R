87298a2ced7a2b224253748871f0a659dc8428c2
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-09-06 11:50:20 +0200

    corrected script for pheromone dtw

--- running_code/Simulations/CL_pheromone_dtw.R
+++ running_code/Simulations/CL_pheromone_dtw.R
@@ -4,7 +4,7 @@ library(data.table)
 source('~/research/AnTracks/src/simulations.R')
 load('~/research/AnTracks/data/det_ref_seqs.RData')
 
-dt <- read_gainsOutput('~/research/AutomatAnts/results/pheromone/')
+dt <- read_FilteredOutput('~/research/AutomatAnts/results/pheromone/', filter = 'pheromone')
 
 aligned_dt <- align_sequences(dt, sim_type = 'pheromone')
 pheromone_dtw <- mass_dtw(aligned_dt, det_NS, align = FALSE, only.average = FALSE)
