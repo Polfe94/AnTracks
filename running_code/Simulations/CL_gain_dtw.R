6472735c29ee88fa2d0d799171638dced1408495
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-09-06 10:12:36 +0200

    going back to the unparallel version of dtw

--- running_code/Simulations/CL_gain_dtw.R
+++ running_code/Simulations/CL_gain_dtw.R
@@ -7,6 +7,6 @@ load('~/research/AnTracks/data/det_ref_seqs.RData')
 dt <- read_gainsOutput('~/research/AutomatAnts/results/gains/')
 
 aligned_dt <- align_sequences(dt, sim_type = 'gain')
-gain_dtw <- pdtw(aligned_dt, det_NS, align = FALSE, only.average = FALSE)
+gain_dtw <- mass_dtw(aligned_dt, det_NS, align = FALSE, only.average = FALSE)
 
 save(gain_dtw, file = '~/research/AutomatAnts/results/gains/outputs/gain_dtw.RData')
