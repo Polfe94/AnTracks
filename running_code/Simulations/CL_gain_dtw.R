2af6edeb309776aea8c0833b7f34de2a47febc80
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-09-06 09:51:39 +0200

    Corrected path

--- running_code/Simulations/CL_gain_dtw.R
+++ running_code/Simulations/CL_gain_dtw.R
@@ -9,4 +9,4 @@ dt <- read_gainsOutput('~/research/AutomatAnts/results/gains/')
 aligned_dt <- align_sequences(dt, sim_type = 'gain')
 gain_dtw <- pdtw(aligned_dt, det_NS, align = FALSE, only.average = FALSE)
 
-save(gain_dtw, file = '~/research/AutomatAnts/results/outputs/gain_dtw.RData')
+save(gain_dtw, file = '~/research/AutomatAnts/results/gains/outputs/gain_dtw.RData')
